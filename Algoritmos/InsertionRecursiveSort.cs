935b615af93231acb64dda32fdcfa8409e4c20f3
Flavio Ribeiro Lima <flavioweb1@hotmail.com>
2021-05-02 18:59:19 -0300

    Add QuickSort, InsertionSort, InsertionRecursiveSort.

---
+++ Algoritmos/InsertionRecursiveSort.cs
@@ -0,0 +1,38 @@
+﻿namespace Algoritmos
+{
+    class InsertionRecursiveSort
+    {
+        public static void DoInsertionRecursiveSort(int[] valores, int begin, int end)
+        {
+            if (begin < end)
+            {
+                int middle = (begin + end) / 2;
+                DoInsertionRecursiveSort(valores, begin, middle);
+                DoInsertionRecursiveSort(valores, middle + 1, end);
+                InsertionList(valores, begin, middle, end);
+            }
+        }
+
+        public static void InsertionList(int[] valores, int begin, int middle, int end)
+        {
+            int left = begin;
+            int shift = 0;
+
+            for (int i = middle + 1; i <= end; i++)
+            {
+                int temp = valores[i];
+
+                for (shift = i; shift > left && valores[shift - 1] > temp; shift--)
+                {
+                    valores[shift] = valores[shift - 1];
+                }
+
+                if (shift != i)
+                {
+                    valores[shift] = temp;
+                }
+                left = shift + 1;
+            }
+        }
+    }
+}
---
+++ Algoritmos/QuickSort.cs
@@ -0,0 +1,49 @@
+﻿namespace Algoritmos
+{
+    class QuickSort
+    {
+        public static void DoQuickSort(int[] valores, int left, int right)
+        {
+            if (valores.Length > 1)
+            {
+                int index = Partition(valores, left, right);
+
+                if (left < index -1)
+                {
+                    DoQuickSort(valores, left, index -1);
+                }
+                if (index < right)
+                {
+                    DoQuickSort(valores, index, right);
+                }
+            }
+        }
+
+        public static int Partition(int[] valores, int left, int right)
+        {
+            int pivot = valores[(right + left) / 2];
+
+            int i = left;
+            int j = right;
+
+            while (i <= j)
+            {
+                while (valores[i] < pivot)
+                {
+                    i++;
+                }
+                while (valores[j] > pivot)
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    Swap.DoSwap(i, j);
+                    i++;
+                    j--;
+                }
+            }
+            return i;
+        }
+    }
+}
